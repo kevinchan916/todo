12e2961003f1d9083e8cef415d2eb2fc3cef15a5
Josh Singh <jashmit04@yahoo.com>
2018-04-11 21:30:33 -0700

    To Do List in Array Structure

--- todo.js
+++ todo.js
@@ -1,1 +1,19 @@
 console.log("Hello TODO");
+var todoList = [];
+
+console.log(todoList);
+function addToList(message) {
+   return todoList.push(message);
+}
+
+function deleteIndexFromList(index) {
+    todoList = todoList.slice(index, index + 1);
+}
+
+function printAllToDo() {
+    todoList.forEach(printMessage);
+}
+
+function printMessage(message) {
+    console.log(message);
+}
